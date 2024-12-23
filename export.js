d770731d262f9f70dc55392348ccb4cf3f55aa9f
Nhan Bui <nhan.bui@coinbase.com>
2024-12-24 00:59:58 +0800

    Revise choices

---
+++ export.js
@@ -0,0 +1,45 @@
+const fsPromise = require("fs/promises");
+const path = require("path");
+const { glob } = require("glob");
+
+(async () => {
+  const translatedFilePaths = await glob("clean-final/*.txt");
+  translatedFilePaths.sort((s1, s2) => (s1 > s2) - (s1 < s2));
+
+  for (const translatedFilePath of translatedFilePaths) {
+    const [, translatedFileName] = translatedFilePath.split("/");
+    const exportedFilePath = `clean-final-json/${translatedFileName.replace(
+      ".txt",
+      ".json"
+    )}`;
+    const translatedFileContent = await fsPromise.readFile(translatedFilePath, {
+      encoding: "utf-8",
+    });
+    const translatedFileLines = translatedFileContent.split("\n");
+    const entries = [];
+    for (const line of translatedFileLines) {
+      const speakerMatch = line.match(/^.*: ".*$/);
+      const validSpeakerMatch = line.match(/^(.*): "(.*)"$/);
+      if (speakerMatch != null && validSpeakerMatch == null) {
+        console.log("INVALID SPEAKER LINE", line);
+        return;
+      }
+
+      if (validSpeakerMatch != null) {
+        entries.push({
+          name: validSpeakerMatch[1],
+          message: validSpeakerMatch[2],
+        });
+      } else {
+        entries.push({
+          message: line,
+        });
+      }
+    }
+
+    await fsPromise.writeFile(
+      exportedFilePath,
+      JSON.stringify(entries, null, 2)
+    );
+  }
+})();
--- verify-choices.js
+++ verify-choices.js
@@ -1,32 +1,24 @@
+const { constants } = require("fs");
 const fsPromise = require("fs/promises");
 const path = require("path");
 const { glob } = require("glob");
 
-const NAME_LIST = [
-  // First names
-  ["拓也", "Takuya"],
-  ["沙耶", "Saya"],
-  ["香織", "Kaori"],
-  ["乙葉", "Otoha"],
-  ["由紀", "Yuki"],
-  ["律子", "Ritsuko"],
-  ["隆司", "Takashi"],
-  ["松太郎", "Matsutarou"],
-  ["昭彦", "Akihiko"],
-  ["時江", "Tokie"],
-  // Last names
-  ["三澤", "Misawa"],
-  ["進藤", "Shindou"],
-  ["沢木", "Sawaki"],
-  ["志田", "Shida"],
-];
-
 (async () => {
-  const translatedFilePaths = await glob("clean-temp/*.txt");
-  translatedFilePaths.sort((s1, s2) => (s1 < s2) - (s1 > s2));
+  const translatedFilePaths = await glob("clean-final/*.txt");
+  translatedFilePaths.sort((s1, s2) => (s1 > s2) - (s1 < s2));
+
   for (const translatedFilePath of translatedFilePaths) {
     const [, translatedFileName] = translatedFilePath.split("/");
-    const originalFilePath = `output/${translatedFileName}`;
+    const choicesFilePath = `choices/${translatedFileName}.json`;
+    const originalFilePath = `otto_output/${translatedFileName}`;
+
+    try {
+      await fsPromise.access(choicesFilePath, constants.F_OK);
+      console.log("_______________________________________________");
+      console.log(`clean-final/${translatedFileName}`, "Choices exist");
+    } catch (err) {
+      continue;
+    }
 
     const originalFileContent = await fsPromise.readFile(originalFilePath, {
       encoding: "utf-8",
@@ -42,6 +34,11 @@ const NAME_LIST = [
       .split("\n")
       .map((s) => s.trim())
       .filter((s) => s.length > 0);
+    const choices = JSON.parse(
+      await fsPromise.readFile(choicesFilePath, {
+        encoding: "utf-8",
+      })
+    );
 
     if (originalFileLines.length !== translatedFileLines.length) {
       console.log(
@@ -55,28 +52,22 @@ const NAME_LIST = [
       break;
     }
 
-    let reviewRequired = false;
     for (let i = 0; i < originalFileLines.length; i++) {
       const originalLine = originalFileLines[i];
       const translatedLine = translatedFileLines[i];
-
-      // Name suffixes check
-      for (const nameEntries of NAME_LIST) {
-        const japaneseNameCount = (
-          originalLine.match(new RegExp(nameEntries[0], "g")) || []
-        ).length;
-        const translatedNameCount = (
-          translatedLine.match(new RegExp(nameEntries[1], "g")) || []
-        ).length;
-
-        if (japaneseNameCount !== translatedNameCount) {
-          reviewRequired = true;
-          console.log(i + 1, nameEntries, originalLine, translatedLine);
+      if (choices.some((choice) => choice === originalLine)) {
+        if (translatedLine.endsWith(".")) {
+          console.log("DOT", i + 1, translatedLine);
+        }
+        if (translatedLine.length > 46) {
+          console.log(
+            "EXCEEDED LIMIT",
+            i + 1,
+            translatedLine.length,
+            translatedLine
+          );
         }
       }
-    }
-    if (reviewRequired) {
-      console.log("FILE NAME:", translatedFileName);
     }
   }
 })();
